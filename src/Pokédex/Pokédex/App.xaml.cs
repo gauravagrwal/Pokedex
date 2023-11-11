e2406dd3b8089a8be62b258671ddfe0ea594929d
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-11-11 23:20:28 +0530

    Add project files.

---
+++ src/Pokédex/Pokédex/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Pokédex
+{
+    public partial class App : Application
+    {
+        public App() => InitializeComponent();
+
+        protected override void OnStart()
+        {
+            MainPage = new Pages.MainPage();
+        }
+
+        protected override void OnSleep() { }
+
+        protected override void OnResume() { }
+    }
+}
---
+++ src/Pokédex/Pokédex/Pages/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace Pokédex.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() => InitializeComponent();
+    }
+}
