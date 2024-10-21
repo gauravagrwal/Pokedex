ba87e72f82afb319e61f5671ef7c2aee62d32628
Gaurav Agrawal <gauravag.1005@gmail.com>
2024-10-21 23:40:03 +0530

    feat: Getting Pokémons from PokéApi

--- src/Constants.cs
+++ src/Constants.cs
@@ -3,4 +3,5 @@
 internal class Constants
 {
     internal const string BASE_URL = "https://pokeapi.co/api/v2/";
+    internal const int LIMIT = 20;
 }
---
+++ src/Helpers/PokédexHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Pokédex.Helpers;
+
+internal static class PokédexHelper
+{
+    internal static int ExtractIdFromUrl(string url)
+    {
+        if (string.IsNullOrEmpty(url))
+            return default;
+
+        if (url.EndsWith("/"))
+            url = url.Remove(url.Length - 1);
+
+        var splitUrl = url.Split("/");
+
+        if (int.TryParse(splitUrl[splitUrl.Count() - 1], out int id))
+            return id;
+
+        return default;
+    }
+}
