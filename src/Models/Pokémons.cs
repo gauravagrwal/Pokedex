0100dda0c918f342f193cdbdb59f3f401c542df9
Gaurav Agrawal <gauravag.1005@gmail.com>
2024-10-21 23:40:03 +0530

    feat: Getting Pokémons from PokéApi

---
+++ src/Models/Pokémons.cs
@@ -0,0 +1,7 @@
+﻿namespace Pokédex.Models;
+
+public class Pokémons
+{
+    public string Name { get; set; }
+    public string Image => $"https://img.pokemondb.net/artwork/{Name}.jpg";
+}
---
+++ src/Models/ResourceListsModel.cs
@@ -0,0 +1,18 @@
+﻿using Pokédex.Helpers;
+
+namespace Pokédex.Models;
+
+public class ResourceItemModel
+{
+    public int Id => PokédexHelper.ExtractIdFromUrl(Url);
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+
+public class ResourceListsModel
+{
+    public int Count { get; set; }
+    public string Next { get; set; }
+    public string Previous { get; set; }
+    public IEnumerable<Pokémons> Results { get; set; }
+}
